238e7202030d1a659ff06eae0a002c24bd14e515
Imil Ametov <charlz_klug@mail.ru>
2024-02-27 13:43:31 +0300

    Added Tcl script for reading c-struct file

---
+++ ch_03/67_binary_data/script.tcl
@@ -0,0 +1,12 @@
+set if [open tstStruct r]
+set d [read $if]
+close $if
+
+binary scan $d "i f2 a*" i f s
+set 0pos [string first [binary format c 0x00] $s]
+incr 0pos -1
+set s [string range $s 0 $0pos]
+
+puts $i
+puts $f
+puts $s
