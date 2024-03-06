06f8e7c50f6943ad8c7805142538bdf02ee5109d
Imil Ametov <charlz_klug@mail.ru>
2024-03-06 10:30:07 +0300

    Chapter 3. Problem 300. Part A

--- ch_03/88_problems/300_problem/source.tcl
+++ ch_03/88_problems/300_problem/source.tcl
@@ -1,8 +1,23 @@
 proc bubble-sort {input_list} {
-    set my_list $input_list
-    bubble-sort {my_list 0 0}
-    return $my_list
+    return [bubble-sort-recur $input_list 0 false]
 }
 
-proc bubble-sort-recur {input_list input_index need-restart?} {
-    
+proc bubble-sort-recur {input_list input_index need_restart} {
+    if {$input_list == {}} {return $input_list}
+    if {$input_index == [llength $input_list] - 1} {
+	if {$need_restart} {
+	    tailcall bubble-sort-recur $input_list 0 false
+	} {
+	    return $input_list
+	}
+    }
+
+    set element1 [lindex $input_list $input_index]
+    set element2 [lindex $input_list [expr {$input_index + 1}]]
+    if {$element1 > $element2} {
+	lset input_list $input_index $element2
+	lset input_list [expr {$input_index + 1}] $element1
+	tailcall bubble-sort-recur $input_list [expr {$input_index + 1}] true
+    }
+    tailcall bubble-sort-recur $input_list [expr {$input_index + 1}] $need_restart
+}
