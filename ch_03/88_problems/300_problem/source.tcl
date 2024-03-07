5b5b96cfe02a90d6f4fea3e8beab7616cc6bcbf4
Imil Ametov <charlz_klug@mail.ru>
2024-03-07 11:54:11 +0300

    Chapter 3. Problem 300. Part B

--- ch_03/88_problems/300_problem/source.tcl
+++ ch_03/88_problems/300_problem/source.tcl
@@ -21,3 +21,25 @@ proc bubble-sort-recur {input_list input_index need_restart} {
     }
     tailcall bubble-sort-recur $input_list [expr {$input_index + 1}] $need_restart
 }
+
+proc bubble-sort-loop {input_list} {
+    set input_list_length [llength $input_list]
+    if {$input_list_length < 2} {return $input_list}
+
+    set sort_one_more_time false
+
+    while true {
+	for {set i 0} {$i < [expr {$input_list_length - 1}]} {incr i} {
+	    set element1 [lindex $input_list $i]
+	    set element2 [lindex $input_list [expr {$i + 1}]]
+	    if {$element1 > $element2} {
+		lset input_list $i $element2
+		lset input_list [expr {$i + 1}] $element1
+		set sort_one_more_time true
+	    }
+	}
+	if {!$sort_one_more_time} break
+	set sort_one_more_time false
+    }
+    return $input_list
+}
