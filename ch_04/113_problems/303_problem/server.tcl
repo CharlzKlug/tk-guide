5b5ce9c1013d9edd0b36d69e94214d5465172fb2
Imil Ametov <charlz_klug@mail.ru>
2024-05-21 10:26:09 +0300

    Chapter 4. Problem no. 303 solved

---
+++ ch_04/113_problems/303_problem/server.tcl
@@ -0,0 +1,31 @@
+#!/usr/bin/env tclsh
+
+proc sum-list {args} {
+    set result 0
+    foreach element $args {
+	incr result $element
+    }
+    if {$result == 30} {set result 31}
+    return $result
+}
+
+proc say {inputChannel inputMessage} {
+    puts $inputChannel $inputMessage
+    flush $inputChannel
+}
+
+proc serverOpen {channel addr port} {
+    puts "New connection: address: $addr, port: $port"
+    dialog $channel
+    close $channel
+}
+
+proc dialog {inputChannel} {
+    set inputLine [gets $inputChannel]
+    say $inputChannel [sum-list {*}$inputLine]
+}
+
+set serverPort 12345
+set server [socket -server serverOpen $serverPort]
+set done 0
+vwait done
