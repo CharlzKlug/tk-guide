aac68ec9bc8cbf1b3488629e1eb1e45bd67c1c2e
Imil Ametov <charlz_klug@mail.ru>
2024-05-21 10:48:12 +0300

    Chapter 5. Urls

---
+++ ch_05/urls/urls.tcl
@@ -0,0 +1,11 @@
+set urls {
+    wiki.tcl.tk "Tcler's Wiki"
+    core.tcl.tk "Tcl/Tk Fossil Core"
+    sourceforge.net/projects/tcl/ "Sourceforge Tcl"
+    sourceforge.net/projects/tktoolkit/ "Sourceforge Tk"
+    www.noucorp.com "Noumena Corporation"
+    www.activestate.com "ActiveState"
+    www.tcl.tk/ "Tcl Developer's Exchange"
+    expect.nist.gov/ "Expect Home Page"
+    www.tclcommunityassociation.org "Tcl/Tk Community Association"
+}
