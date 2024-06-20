fb30e10a67bc5c0d91085d8be1090f6c270f5f7a
Imil Ametov <charlz_klug@mail.ru>
2024-06-20 15:22:48 +0300

    Chapter 6. Problem no. 105 solved

---
+++ ch_06/178_problems/105_problem/solution.tcl
@@ -0,0 +1,4 @@
+#!/usr/bin/env tclsh
+# What Tcl command will report the type of file (a normal file or directory, for example) given a file name?
+puts [file type solution.tcl]
+puts [file type ../]
