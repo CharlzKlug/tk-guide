f7d0ec90a5f7ff0b9a67430b5b369b6a7a3b3e2c
Imil Ametov <charlz_klug@mail.ru>
2024-07-10 15:24:05 +0300

    Chapter 7. Problem no. 303 solved

--- ch_07/201_problems/303_problem/solution.tcl
+++ ch_07/201_problems/303_problem/solution.tcl
@@ -20,4 +20,34 @@ proc proc_sum {inputProcName} {
     return [string_code [info body $inputProcName]]
 }
 
+proc proc_sum2 {inputProcName} {
+    return [string_code [info body $inputProcName]]
+}
 
+proc proc_sum3 {inputProcName} {
+    return [string_code [info body $inputProcName]]
+}
+
+proc search_equal_procs_iter {inputProcsName outputResultList} {
+    if {[llength $inputProcsName] < 2} {return $outputResultList} {
+	set sampleProcName [lindex $inputProcsName 0]
+	set reducedProcsNameList [lrange $inputProcsName 1 end]
+	set newProcsNameList {}
+	set innerResult [list $sampleProcName]
+	foreach procName $reducedProcsNameList {
+	    if {[info body $sampleProcName] == [info body $procName]} {
+		lappend innerResult $procName
+	    } {
+		lappend newProcsNameList $procName
+	    }
+	}
+	if {[llength $innerResult] > 1} {
+	    lappend outputResultList $innerResult
+	}
+	search_equal_procs_iter $newProcsNameList $outputResultList
+    }
+}
+
+proc search_equal_procs {} {
+    return [search_equal_procs_iter [info procs] {}]
+}
