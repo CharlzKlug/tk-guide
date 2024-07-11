576aa5d2a49efb451ed9aaab7ddda8159c7d5964
Imil Ametov <charlz_klug@mail.ru>
2024-07-11 10:03:16 +0300

    Chapter 7. Problem no. 306 started

---
+++ ch_07/201_problems/306_problem/solution.tcl
@@ -0,0 +1,15 @@
+# Write two procedures, as follows.
+# Syntax: class className body
+# Adds a new class name to a collection of known classes and associates the body with that class.
+# Syntax: new className value
+# Creates a variable with a unique name in the global scope, and assigns value to it. Creates a new procedure with the same name and single argument args, and uses the body that was defined with the class command as the body for the procedure.
+# When complete, code such as follows should function.
+# class test {return "args are: $args"}
+# set x [new test 22]
+# puts "Value of test: [set $x]"
+# puts "Results of test: [$x a b c]"
+# The script output would look as follows.
+
+# Script Output
+# Value of test: 22
+# Results of test: args are: a b c
