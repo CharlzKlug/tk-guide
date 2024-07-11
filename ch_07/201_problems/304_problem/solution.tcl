2855f5d223c816bc2dede9dce1520abba039e712
Imil Ametov <charlz_klug@mail.ru>
2024-07-11 09:24:56 +0300

    Chapter 7. Problem no. 304 has been skipped

---
+++ ch_07/201_problems/304_problem/solution.tcl
@@ -0,0 +1,3 @@
+# Write a procedure for the tree.tcl module described in Chapter 6 that will return a list of a node siblings (the other child nodes to this node parent). Add a new method to the treeObjectProc described in this chapter to evaluate the treeGetSibling procedure.
+
+# I can not find any tree.tcl module described in Chapter 6. The problem has been skipped.
