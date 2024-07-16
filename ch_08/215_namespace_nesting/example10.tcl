28a79db018148ce668031468f8b902765ff0f113
Imil Ametov <charlz_klug@mail.ru>
2024-07-16 17:25:44 +0300

    Chapter 8. Namespace Nesting. Example 10

---
+++ ch_08/215_namespace_nesting/example10.tcl
@@ -0,0 +1,95 @@
+#!/usr/bin/env tclsh
+
+set stackDef {
+    variable stack {}
+    proc push {value} {
+	variable stack
+	lappend stack $value
+    }
+
+proc pop {} {
+    variable stack
+    set rtn [lindex $stack end]
+    set stack [lrange $stack 0 end-1]
+    return $rtn
+}
+
+proc peek {{pos end}} {
+    variable stack
+    return [lindex $stack $pos]
+}
+
+proc size {} {
+    variable stack
+    return [llength $stack]
+}
+}
+
+namespace eval Hanoi {
+    namespace eval left $stackDef
+namespace eval center $stackDef
+namespace eval right $stackDef
+
+######
+# proc moveRing {from to} --
+#   Move the last element of the "from" stack
+#   to the end of the "to" stack.
+#
+# Arguments
+#   from: The name of the stack to move from.
+#   to: The name of the stack to move to.
+# Results
+#   The "from" and "to" stacks are modified.
+proc moveRing {from to} {
+    ${to}::push [${from}::pop]
+}
+
+proc show {} {
+    puts ""
+    for {set p 4} {$p >= 0} {incr p -1} {
+	set out ""
+	foreach stack {left center right} {
+	    set ring [${stack}::peek $p]
+	    if {$ring ne ""} {
+		set l [format %5s [string repeat - $ring]]
+		set r [format %-5s [string repeat - $ring]]
+	    } else {
+		set l [format %5s " "]
+		set r $l
+	    }
+	    append out [format %s%s%s $l "||" $r]
+	}
+	puts $out
+    }
+    puts ""
+}
+
+proc start {} {
+    variable left
+    # Fill the left stack
+    for {set i 4} {$i > 0} {incr i -1} {
+	left::push $i
+    }
+}
+
+proc done {} {
+    if {[right::size] == 4} {
+	return 1
+    } else {
+	return 0
+    }
+}
+}
+Hanoi::start
+
+array set abbr {l left r right c center}
+while {![Hanoi::done]} {
+Hanoi::show
+puts -nonewline "From (lcr): "
+flush stdout
+set from [gets stdin]
+puts -nonewline "To (lcr): "
+flush stdout
+set to [gets stdin]
+Hanoi::moveRing $abbr($from) $abbr($to)
+}
