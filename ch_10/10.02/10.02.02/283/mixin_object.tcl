e7058cb6202529e3dcbe3b94806e63820d497b12
Imil Ametov <charlz_klug@mail.ru>
2025-03-06 15:49:21 +0300

    Defining Per-object Mixins

---
+++ ch_10/10.02/10.02.02/283/mixin_object.tcl
@@ -0,0 +1,43 @@
+#!/usr/bin/env tclsh
+
+oo::class create magicHelmet {
+    method defense {attackStrength} {
+	puts "Magic Helmet Reduces attack by 2"
+	return [next [expr {$attackStrength - 2}]]
+    }
+}
+
+oo::class create spear {
+    method defense {attackStrength} {
+	puts "Spear reduces attack by 2"
+	return [next [expr {$attackStrength - 2}]]
+    }
+}
+
+oo::class create warrior {
+    mixin magicHelmet
+
+    constructor {} {
+	variable State
+	array set State {defense 4 attack 4 hitpoints 5}
+    }
+
+    method defense {attackStrength} {
+	variable State
+	puts "Final Attack is: $attackStrength"
+	if {$attackStrength > $State(defense)} {
+	    return "Hit"
+	} else {
+	    return "Missed"
+	}
+    }
+}
+
+warrior create elmer
+
+puts "With only a magic helmet"
+puts "With a magic helmet elmer is [elmer defense 8]"
+
+puts "\n Add a spear to the elmer object"
+oo::objdefine elmer mixin spear
+puts "With spear and magic helmet elmer is [elmer defense 8]"
