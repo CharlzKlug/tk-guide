b302b112dc34a8bd32472c32771a71746981b987
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-05-18 00:37:08 +0300

    Some change in 11.04

--- ch_11/11.04/example_04.tcl
+++ ch_11/11.04/example_04.tcl
@@ -2,5 +2,6 @@
 # \
     exec wish "$0" "$@"
 
-set clickButton [button .b1 -text "Please Click Me" -command {.b1 configure -text "I've been Clicked!"}]
+set clickButton [button .b1 -text "Please Click Me" \
+		     -command {.b1 configure -text "I've been Clicked!"}]
 grid $clickButton
