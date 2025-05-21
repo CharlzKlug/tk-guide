7c3d2df0d945b84059ed8c7a149be28b199bd479
Imil Ametov <charlz_klug@mail.ru>
2025-05-21 10:10:21 +0300

    The labelframe Widget

---
+++ ch_11/11.07/11.07.01/342_labelframe_widget/example_15.tcl
@@ -0,0 +1,42 @@
+#!/bin/sh
+# \
+    exec wish "$0" "$@"
+
+set infoFrame [frame .info]
+set buttonFrame [frame .buttons]
+set mainFrame [frame .main -relief solid -borderwidth 2]
+set statusFrame [frame .status]
+
+set w [label $infoFrame.name -text "Sample Application" \
+	   -font {arial 16 bold}]
+grid $w
+set w [label $infoFrame.version -text "Revision 1.0"]
+grid $w
+
+foreach buttonName {File Edit Help} {
+    lappend buttonList [button $buttonFrame.b_$buttonName \
+			    -text $buttonName -command "perform_$buttonName"]
+}
+
+grid {*}$buttonList
+
+foreach id {Name Street City State} {
+    set w [labelframe $mainFrame.l_$id -text $id:]
+    set w2 [entry $w.e_$id -textvariable State($id) \
+		-background white]
+    grid $w
+    grid $w2
+}
+
+set w1 [label $statusFrame.status -textvariable status]
+set w2 [label $statusFrame.time -textvariable time]
+
+grid $w1 $w2
+
+set status "No Errors"
+set time [clock format [clock seconds]]
+
+grid $infoFrame
+grid $buttonFrame -sticky w
+grid $mainFrame -sticky ew
+grid $statusFrame
