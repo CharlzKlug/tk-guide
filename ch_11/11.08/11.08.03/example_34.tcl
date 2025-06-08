bbf8286022b6655158a6e08994fcb9d5b4480586
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2025-06-08 23:42:00 +0300

    Example 34

---
+++ ch_11/11.08/11.08.03/example_34.tcl
@@ -0,0 +1,14 @@
+#!/bin/sh
+# \
+    exec wish "$0" "$@"
+
+# Create and display an empty listbox
+listbox .l -height 3
+pack .l
+# Add 3 elements to the listbox
+# Note - insert at end position - order is as expected
+.l insert end first
+.l insert end second
+.l insert end third
+
+bind .l {<<ListboxSelect>>} {puts "Selected Text: [.l get [.l curselection]]"}
