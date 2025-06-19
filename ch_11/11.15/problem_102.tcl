b2b240b290564b6652032c7d2cf284cc02a0483b
Imil Ametov <charlz_klug@mail.ru>
2025-06-19 09:36:31 +0300

    Problems 102 solved

--- ch_11/11.15/problem_102.tcl
+++ ch_11/11.15/problem_102.tcl
@@ -2,7 +2,6 @@
 # \
     exec wish "$0" "$@"
 
-button .exit_button -text "QUIT" -command exit
-.exit_button configure -background #FF0000
-.exit_button configure -activebackground #00FF00
+# 102 What conventions does Tcl use to define screen distances?
+button .exit_button -text "QUIT" -command exit -borderwidth 3p
 grid .exit_button
