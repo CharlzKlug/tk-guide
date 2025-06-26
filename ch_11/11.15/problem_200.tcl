2d251e64972dba3af94694a847730639b4275c6e
Imil Ametov <charlz_klug@mail.ru>
2025-06-26 16:28:48 +0300

    Problem 200 in progress

---
+++ ch_11/11.15/problem_200.tcl
@@ -0,0 +1,21 @@
+#!/bin/sh
+# \
+    exec wish "$0" "$@"
+
+proc pigLatin {inputWord} {
+    set headLetter [string index $inputWord 0]
+    set latinWord [string replace $inputWord 0 0]
+    if {[string match -nocase {[aeiouy]} $headLetter]} {
+        set latinWord ${latinWord}t
+    }
+    set latinWord ${latinWord}${headLetter}
+    set latinWord ${latinWord}ay
+    return $latinWord
+}
+
+entry .entry
+label .lbl -text "No text"
+button .btn -text "To label"
+grid .entry
+grid .lbl
+grid .btn
