f070c4da7f2a379531f78d20b9076b221884d0cb
Imil Ametov <charlz_klug@mail.ru>
2025-06-27 17:58:05 +0300

    Problem 201 solved

---
+++ ch_11/11.15/problem_201.tcl
@@ -0,0 +1,28 @@
+#!/bin/sh
+# \
+    exec wish "$0" "$@"
+
+array set colorcount {
+    0 #000000
+    1 #0000FF
+    2 #00FF00
+    3 #00FFFF
+    4 #FF0000
+    5 #FF00FF
+    6 #FFFF00
+    7 #FFFFFF
+}
+
+set currentColor 0
+
+proc switchColor {} {
+    uplevel 1 {
+        .btn configure -background $colorcount($currentColor)
+        .btn configure -activebackground $colorcount($currentColor)
+        incr currentColor
+        if {$currentColor > 7} {set currentColor 0}
+    }
+}
+button .btn -text "Click me!" -command \
+    "switchColor"
+grid .btn
